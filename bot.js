cc78e5831ba2e08d8ddd12e11177a054a66f06fb
rroggerr <rroggerr@users.noreply.github.com>
2017-03-24 10:13:30 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -100,11 +100,11 @@ function evaluateIntent(){
 			retval = "It is 68 C with a chance of thermal paste right here in the server! We'll be expecting cooler temperatures at night when the usage is lower!";
 			for (var i = 0; i < callresponse.entities.length; i++) {
 				if (callresponse.entities[i].type!="builtin.geography.city"||callresponse.entities[i].type!="builtin.geography.country"){
-					var capitalized = callresponse.entities.entity.string.charAt(0).toUpperCase();
+					var capitalized = callresponse.entities.entity.charAt(0).toUpperCase();
 					retval = capitalized.concat(" isn't a city you ignorant person!");
 				}
 				if (callresponse.entities[i].type=="builtin.geography.city"){
-					var capitalized = callresponse.entities.entity.string.charAt(0).toUpperCase();
+					var capitalized = callresponse.entities.entity.charAt(0).toUpperCase();
 					var weatherjson;
 					$.getJSON("http://api.openweathermap.org/data/2.5/weather?APPID=8b235727629642db2c474ae09716141d&units=metric&q=",function(json){
 						weatherjson = json;
