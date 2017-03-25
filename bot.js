6feccd59b68f071ac4d294d9320eca9e09f0b694
rroggerr <rroggerr@users.noreply.github.com>
2017-03-25 18:10:37 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -107,7 +107,7 @@ function evaluateIntent(){
 				}
 				if (callresponse.entities[i].type=="builtin.geography.city"){
 					var weatherlink = "https://www.wunderground.com/cgi-bin/findweather/getForecast?query=".concat(capitalized);
-					retval = "Now do I look like Siri to you? Go find your own weather here you lazy bum! ".concat(weatherlink);
+					retval = "Now do I look like Siri to you? Go find your own weather <a href=/"".concat(weatherlink).concat("\">here </a> you lazy bum! ";
 				}
 			}
 			return retval;
