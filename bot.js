a42db6cc880fc8bf50a1881fc6cf29e9c745be9b
rroggerr <rroggerr@users.noreply.github.com>
2017-03-25 18:42:35 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -119,7 +119,7 @@ function evaluateIntent(){
 					retval = 'Now do I look like Siri to you? Go find the weather for '.concat(capitalized).concat(' <a href="').concat(weatherlink).concat('">here </a> you lazy bum!');
 				}
 				if (callresponse.entities[i].type=="builtin.datetime.date" && callresponse.entities[i].entity!='today'){
-					retval = "Do I look like I have a time machine? Even if I did, I wouldn't use it to check the weather!");
+					retval = "Do I look like I have a time machine? Even if I did, I wouldn't be using it to check the weather!";
 				}										       
 			}
 			return retval;
