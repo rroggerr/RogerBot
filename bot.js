08833d1042aed3a2dbd94a4396d8574086e46105
rroggerr <rroggerr@users.noreply.github.com>
2017-03-27 10:39:50 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -114,6 +114,13 @@ function evaluateIntent(){
 					retval= 'I was created by Roger of course!';
 					break;
 				}
+				// Entities override school + job
+				if (callresponse.entities.[i].type=="question::School"){
+					retval = "School? That's nonsense! I already know everything there is to know!"
+				}
+				else if (callresponse.entities.[i].type=="question::Job"){
+					retval="This is my job! Getting paid minimum wage to answer your stupid questions!"
+				}
 			}
 			return retval;
 		case 4: // About Roger
